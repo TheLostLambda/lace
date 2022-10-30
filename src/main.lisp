8de2debcbb7722e5f0957173281d637318c05309
Brooks J Rady <b.j.rady@gmail.com>
2022-10-30 12:58:43 +0100

    REPL?

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,18 @@
 (defpackage llace
-  (:use :cl))
+  (:use :cl)
+  (:export :repl))
 (in-package :llace)
 
-;; blah blah blah.
+(defun repl ()
+  (loop (lc-print (lc-eval (lc-read)))))
+
+(defun lc-print (x) x)
+(defun lc-eval (x) x)
+
+(defun lc-read ()
+  (scan (read-line)))
+
+(defun scan (str)
+  (print str)
+  str)
+
